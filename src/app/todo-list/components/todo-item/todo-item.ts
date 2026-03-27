12fed61980545fd7860adb795d3052beb00bf3c4
Julio Saura <elcorreoesdejulio@gmail.com>
2026-03-27 19:24:00 +0100

    feat(todo-item): GREEN - implement TodoItemComponent with controlled checkbox, priority badge and a11y

--- src/app/todo-list/components/todo-item/todo-item.ts
+++ src/app/todo-list/components/todo-item/todo-item.ts
@@ -1,5 +1,6 @@
-import { ChangeDetectionStrategy, Component, input, output } from '@angular/core';
+import { ChangeDetectionStrategy, Component, computed, input, output } from '@angular/core';
 import { Todo } from '@interfaces/Todo';
+import { PriorityLabel } from '@enums/Priority';
 
 @Component({
   selector: 'app-todo-item',
@@ -11,4 +12,15 @@ export class TodoItemComponent {
   readonly todo = input.required<Todo>();
   readonly toggle = output<string>();
   readonly delete = output<string>();
+
+  readonly priorityLabel = computed(() => PriorityLabel[this.todo().priority]);
+
+  onToggle(event: Event): void {
+    event.preventDefault();
+    this.toggle.emit(this.todo().id);
+  }
+
+  onDelete(): void {
+    this.delete.emit(this.todo().id);
+  }
 }
