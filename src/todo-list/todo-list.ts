4d665b0a85e33f2c6f885320c4cd6da1fffa437c
Julio Saura <elcorreoesdejulio@gmail.com>
2026-03-27 16:03:45 +0100

    refactor(todo-list): remove unused CommonModule import for Angular 21+ standalone

--- src/todo-list/todo-list.ts
+++ src/todo-list/todo-list.ts
@@ -1,9 +1,9 @@
-import {  Component, signal, effect } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { Component, effect, signal } from '@angular/core';
+
 
 @Component({
   selector: 'app-todo-list',
-  imports: [CommonModule],
+  imports: [],
   templateUrl: './todo-list.html',
 })
 export class TodoListComponent  {
