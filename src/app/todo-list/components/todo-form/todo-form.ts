cb65008950dde1a5564bff931dbd4cd9f88db052
Julio Saura <elcorreoesdejulio@gmail.com>
2026-03-27 19:35:29 +0100

    feat(todo-form): GREEN - implement form with NonNullableFormBuilder, dynamic priority options and a11y

--- src/app/todo-list/components/todo-form/todo-form.ts
+++ src/app/todo-list/components/todo-form/todo-form.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { ReactiveFormsModule, NonNullableFormBuilder, Validators } from '@angular/forms';
+import { Priority, PriorityLabel } from '@enums/Priority';
 
 @Component({
   selector: 'app-todo-form',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './todo-form.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class TodoForm {
+  readonly #fb = inject(NonNullableFormBuilder);
 
+  readonly priorityOptions = Object.entries(PriorityLabel).map(([value, label]) => ({
+    value: Number(value) as Priority,
+    label,
+  }));
+
+  readonly form = this.#fb.group({
+    title: ['', Validators.required],
+    priority: [Priority.MEDIUM],
+  });
 }
