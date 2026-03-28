1236956363a873a51edc2d0add2c09af9dc57931
Julio Saura <elcorreoesdejulio@gmail.com>
2026-03-28 20:47:59 +0100

    feat(todo-filter): GREEN - implement filter buttons with computed active states, titlecase pipe and host a11y

--- src/app/todo-list/components/todo-filter/todo-filter.ts
+++ src/app/todo-list/components/todo-filter/todo-filter.ts
@@ -1,11 +1,24 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, computed, input } from '@angular/core';
+import { TitleCasePipe } from '@angular/common';
+import { TodoFilter as TodoFilterEnum } from '@enums/TodoFilter';
 
 @Component({
   selector: 'app-todo-filter',
-  imports: [],
+  imports: [TitleCasePipe],
   templateUrl: './todo-filter.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
+  host: {
+    class: 'flex items-center gap-2 p-2',
+    role: 'group',
+    'aria-label': 'Filter tasks',
+  },
 })
 export class TodoFilter {
+  readonly activeFilter = input<TodoFilterEnum>(TodoFilterEnum.ALL);
+  readonly filters: TodoFilterEnum[] = Object.values(TodoFilterEnum);
 
+  readonly activeStates = computed(() => {
+    const active = this.activeFilter();
+    return Object.fromEntries(this.filters.map(filter => [filter, filter === active])) as Record<TodoFilterEnum, boolean>;
+  });
 }
