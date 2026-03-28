c6e28d33cbafc0d96381ee56d072d7b842ef307e
Julio Saura <elcorreoesdejulio@gmail.com>
2026-03-28 19:52:38 +0100

    feat(todo-form): GREEN - implement title validation with maxlength, whitespace-only and space collapse

--- src/app/todo-list/components/todo-form/todo-form.ts
+++ src/app/todo-list/components/todo-form/todo-form.ts
@@ -1,6 +1,9 @@
 import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { ReactiveFormsModule, NonNullableFormBuilder, Validators } from '@angular/forms';
 import { Priority, PriorityLabel } from '@enums/Priority';
+import { MULTI_SPACE } from '@constants/multi-space';
+import { noWhitespaceOnly } from '@validators/no-whitespace-only';
+import { TITLE_MAX_LENGTH } from '../../constants/title-max-length';
 
 @Component({
   selector: 'app-todo-form',
@@ -10,6 +13,7 @@ import { Priority, PriorityLabel } from '@enums/Priority';
 })
 export class TodoForm {
   readonly #fb = inject(NonNullableFormBuilder);
+  protected readonly titleMaxLength = TITLE_MAX_LENGTH;
 
   readonly priorityOptions = Object.entries(PriorityLabel).map(([value, label]) => ({
     value: Number(value) as Priority,
@@ -17,7 +21,13 @@ export class TodoForm {
   }));
 
   readonly form = this.#fb.group({
-    title: ['', Validators.required],
+    title: ['', [Validators.required, Validators.maxLength(this.titleMaxLength), noWhitespaceOnly]],
     priority: [Priority.MEDIUM],
   });
+
+  onTitleInput(event: Event): void {
+    const input = event.target as HTMLInputElement;
+    input.value = input.value.replace(MULTI_SPACE, ' ');
+    this.form.controls.title.setValue(input.value);
+  }
 }
