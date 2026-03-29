9a7483fd31a6c72c0847ae6cf6c92c26562167e5
Julio Saura <elcorreoesdejulio@gmail.com>
2026-03-29 12:20:27 +0200

    style(layout): add fixed width, title truncate with tooltip and scrollable items container

--- src/app/app.ts
+++ src/app/app.ts
@@ -5,6 +5,6 @@ import { TodoListComponent } from './todo-list/todo-list';
   selector: 'app-root',
   imports: [TodoListComponent],
   templateUrl: './app.html',
-  host: { class: 'w-full p-2 flex flex-col items-center' },
+  host: { class: 'w-full p-2 flex flex-col items-center flex-1 min-h-0' },
 })
 export class App {}
